30fa6d16a1a13437d5feb79b9c5d8f1b99c5f953
archanaa007 <shrivastavarchana804.as@gmail.com>
2025-10-19 20:54:03 +0530

    Currency Rate Converter Changes

--- src/test/groovy/pl/cleankod/BaseApplicationSpecification.groovy
+++ src/test/groovy/pl/cleankod/BaseApplicationSpecification.groovy
@@ -6,7 +6,7 @@ import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.methods.HttpUriRequest
 import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.http.util.EntityUtils
-import pl.cleankod.exchange.entrypoint.model.ApiError
+import pl.cleankod.model.exception.ApiError
 import spock.lang.Specification
 
 abstract class BaseApplicationSpecification extends Specification {
